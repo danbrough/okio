731642ac5e763b04781d4540c773c5ab907057d7
Dan Brough <dan@danbrough.org>
2022-08-22 15:17:55 +1200

    update

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ export JDK_8=/opt/jdk/jdk8/
 if [ "$(uname)" = "Darwin" ]; then
 	./gradlew `cat mac_targets.txt`
 else 
-./gradlew publishAllPublicationsToSonatypeRepository
+./gradlew publishAllPublicationsToMavenCentralRepository
 fi
 
 
