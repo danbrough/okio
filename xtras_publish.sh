9337818e01b2607a5d845eafb87fa66dbc0c3c6b
Dan Brough <dan@danbrough.org>
2022-11-10 20:03:56 +1300

    update

---
+++ xtras_publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd `dirname "$0"`
+
+TASKS="publishAllPublicationsToMavenCentralRepository"
+[ -z "$SONATYPE_USER" ] && echo SONATYPE_USER not set && exit 1
+[ -z "$SONATYPE_PASSWORD" ] && echo SONATYPE_PASSWORD not set && exit 1
+[ -z "$SONATYPE_REPO_ID" ] && echo SONATYPE_REPO_ID not set && exit 1
+
+
+
+OPTS="-PSONATYPE_REPO_ID=$SONATYPE_REPO_ID \
+ -PmavenCentralUsername=$SONATYPE_USER -PmavenCentralPassword=$SONATYPE_PASSWORD \
+ -PsignPublications -PpublishDocs "
+
+if [ "$(uname)" = "Darwin" ]; then
+	TASKS="$(cat mac_targets.txt)"
+fi
+
+./gradlew $OPTS $TASKS
+
+
